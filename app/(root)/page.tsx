adce7f5931a65aa7d61d550151fa08501ec451f3
SagnikGhosh18 <sagnikghosh18@gmail.com>
2024-12-02 22:52:02 +0530

    Setup sanity config

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,42 @@
+import SearchForm from '@/components/SearchForm';
+import StartupCard, { StartupTypeCard } from '@/components/StartupCard';
+import { client } from '@/sanity/lib/client';
+import { STARTUPS_QUERY } from '@/sanity/lib/queries';
 
-export default function Home() {
-  return (
-    <>
-      Home
-    </>
-  );
+export default async function Home({
+    searchParams,
+}: {
+    searchParams: Promise<{ query?: string }>;
+}) {
+    const query = (await searchParams).query;
+    const posts = await client.fetch(STARTUPS_QUERY);
+
+    return (
+        <>
+            <section className="pink_container">
+                <h1 className="heading">
+                    Pitch your startup, <br /> Connect with Extrepreneurs
+                </h1>
+                <p className="sub-heading !max-w-3xl">
+                    Submit Ideas, Vote on Pitcher, and Get Notices in Virtual
+                    Competitions
+                </p>
+                <SearchForm query={query} />
+            </section>
+            <section className="section_container">
+                <p className="text-30-semibold">
+                    {query ? `Search results for ${query}` : `All Startups`}
+                </p>
+                <ul className="mt-7 card_grid">
+                    {posts?.length > 0 ? (
+                        posts.map((post: StartupTypeCard) => (
+                            <StartupCard key={post?._id} post={post} />
+                        ))
+                    ) : (
+                        <p className="no-results">No startups found</p>
+                    )}
+                </ul>
+            </section>
+        </>
+    );
 }
