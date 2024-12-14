10234fce21b946678e8981bae04240921beb8ef9
SagnikGhosh18 <sagnikghosh18@gmail.com>
2024-12-14 15:18:56 +0530

    Adds user profile page

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,56 +1,71 @@
-import { auth, signIn, signOut } from '@/auth'
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
+import { auth, signIn, signOut } from '@/auth';
+import { BadgePlus, LogOut } from 'lucide-react';
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar';
 
 const NavBar = async () => {
     const session = await auth();
 
     return (
-        <header className='px-5 py-3 bg-white shadow-sm font-work-sans'>
-            <nav className='flex justify-between items-center'>
-                <Link href='/'>
+        <header className="px-5 py-3 bg-white shadow-sm font-work-sans">
+            <nav className="flex justify-between items-center">
+                <Link href="/">
                     <Image
-                        src={"/logo.png"}
-                        alt='logo'
+                        src={'/logo.png'}
+                        alt="logo"
                         width={144}
                         height={30}
                     />
                 </Link>
 
-                <div className='flex items-center gap-5 text-black'>
-                    {(session && session?.user) ?
-                        (<>
+                <div className="flex items-center gap-5 text-black">
+                    {session && session?.user ? (
+                        <>
                             <Link href="/startup/create/">
-                                <span>Create</span>
+                                <span className="max-sm:hidden">Create</span>
+                                <BadgePlus className="size-6 sm:hidden text-red-500" />
                             </Link>
                             <form
                                 action={async () => {
-                                    "use server"
-                                    await signOut({ redirectTo: "/" })
+                                    'use server';
+                                    await signOut({ redirectTo: '/' });
                                 }}
                             >
-                                <button type="submit">Sign Out</button>
+                                <button type="submit">
+                                    <span className="max-sm:hidden">
+                                        Log Out
+                                    </span>
+                                    <LogOut className="size-6 sm:hidden text-red-500" />
+                                </button>
                             </form>
                             <Link href={`/user/${session?.id}`}>
-                                <span>{session?.user?.name}</span>
+                                <Avatar className="size-10">
+                                    <AvatarImage
+                                        src={session?.user?.image || ''}
+                                        alt={session?.user?.name || ''}
+                                    />
+                                    <AvatarFallback>AV</AvatarFallback>
+                                </Avatar>
                             </Link>
-                        </>) :
-                        (<>
+                        </>
+                    ) : (
+                        <>
                             <form
                                 action={async () => {
-                                    "use server"
-                                    await signIn("github")
+                                    'use server';
+                                    await signIn('github');
                                 }}
                             >
                                 <button type="submit">Sign In</button>
                             </form>
-                        </>)
-                    }
+                        </>
+                    )}
                 </div>
             </nav>
         </header>
-    )
-}
+    );
+};
 
-export default NavBar
+export default NavBar;
